381af9bb06ff1b3bbaa759c6ab26efd292a76db1
Pierre-Arthur DEMENGEL <124669857+pierrearthurdemengel@users.noreply.github.com>
2024-01-10 17:49:54 +0100

    first commit

---
+++ test.js
@@ -0,0 +1,5 @@
+// Déclarer une variable typée
+var variable;
+// Exemples
+var heroName = 'Green Lantern';
+var healthPoint = 100;
---
+++ test.ts
@@ -0,0 +1,7 @@
+function crearteHeros(name: string, healthPoint: number, originPlanet?: string): Heros {
+    var heros = new Heros();
+    heros.pointDeVie = pointDeVie;
+    heros.name = name;
+
+    return heros;
+} 
